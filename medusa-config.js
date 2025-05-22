9a3ce434f6abad6ed14bdcb0eeb1b1a73d3f58a7
crisjanz <crisjanz@gmail.com>
2025-05-22 14:28:45 -0700

    Create medusa-config.js

---
+++ medusa-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  projectConfig: {
+    redis_url: process.env.REDIS_URL,
+    database_url: process.env.DATABASE_URL,
+    database_type: "postgres",
+    store_cors: process.env.STORE_CORS,
+    admin_cors: process.env.ADMIN_CORS,
+  },
+  plugins: [],
+};
